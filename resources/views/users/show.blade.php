1675ff82dba0fad9e197619502da87178919d7d5
egu33 <eguthi10@gmail.com>
2020-10-16 17:17:58 +0900

    fix view

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,6 @@
   @include('nav')
   <div class="container">
     @include('users.user')
-    <?php echo "こんにちは{$user->name}" ; ?>
     @include('users.tabs', ['hasArticles' => true, 'hasLikes' => false])
     @foreach($articles as $article)
       @include('articles.card')
